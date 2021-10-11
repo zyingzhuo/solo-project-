e023cd9ddff4c21bb6a855be790ef81c6843ee2d
Yingjia Zhuo <yingjiazhuo@Yingjias-MacBook-Pro.local>
2021-10-11 13:32:43 -0700

    seeded all

--- authenticate-me/backend/db/seeders/20211011200148-defaultImages.js
+++ authenticate-me/backend/db/seeders/20211011200148-defaultImages.js
@@ -5,17 +5,23 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Images', [{
       spotId:1,
-      url:faker.image.image(),
+      url:'https://a0.muscache.com/im/pictures/27959272/b1cc8e1d_original.jpg?im_w=1200',
       createdAt: new Date(),
       updatedAt: new Date()
     },
 
     {
-
+      spotId:2,
+      url:'https://a0.muscache.com/im/pictures/4c34b726-ebf0-404f-9425-82a252c03eeb.jpg?im_w=1200',
+      createdAt: new Date(),
+      updatedAt: new Date()
     },
 
     {
-      
+      spotId:3,
+      url:"https://a0.muscache.com/im/pictures/88f9b0c4-489d-4dc5-93ca-0564dbcf7d35.jpg?im_w=1200",
+      createdAt: new Date(),
+      updatedAt: new Date()
     }
   ], {});
     /*
